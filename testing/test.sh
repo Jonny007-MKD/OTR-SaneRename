57e65b53e816477a817a4600df2a535d15dff01a
Jonny007-MKD <nospam@jonny007-mkd.de>
2015-03-12 22:55:34 +0100

    Link EPG in main dir to testing dir. This way we can use old files

--- testing/test.sh
+++ testing/test.sh
@@ -9,19 +9,39 @@ files=(
 );
 
 if [ -f test.sh ]; then
-	sr="../saneRenamix.sh"
+	path=".."
 else
-	sr="./saneRenamix.sh"
+	path="."
+fi
+
+if [ ! -f "$path/saneRenamix.sh" ]; then
+	echo "saneRenamix.sh not found!" >&2
 fi
 
 
-i=0
-for file in "${!files[@]}"; do
-	echo -e "\033[37m${file}";
-	result="$($sr $srArgs -s -f "$file")";
-	if [ "$result" != "${files["$file"]}" ]; then
-		echo -e "\033[31m$file -> $result";
-		echo "'$result' != '${files[$file]}'";
+for the_file in "${!files[@]}"; do
+	echo -e "\033[37m${the_file}";
+
+	file="${the_file//_/ }"
+
+	file_title=${file%% [0-9][0-9].*}						# Cut off everything after the title: date, hour, sender, ...
+	file_sender=${file##*-[0-9][0-9]}						# Cut off everything bevor the sender: title, date, time, ...
+
+	file_date=${file%%$file_sender}							# Cut off the sender
+	file_date=${file_date##$file_title }					# Cut off the title, now we do have the date and time
+	file_time=${file_date##* }
+	file_date=${file_date%% *}
+
+	epg_file="epg-$file_date.csv"
+
+	if ! [ -f $path/$epg_file ]; then				# Create EPG file if neccessary
+		ln -s "testing/$epg_file" "$path/$epg_file"
 	fi
-	i=$(($i+1));
+
+	result="$($path/saneRenamix.sh $srArgs -s -f "$the_file")";
+
+	if [ "$result" != "${files["$the_file"]}" ]; then
+		echo -e "\033[31m$the_file -> $result";
+		echo "'$result' != '${files[$the_file]}'";
+	fi
 done;

a00ba45b48e9449b4f3b566ce8f6638542f4faec
Jonny007-MKD <nospam@jonny007-mkd.de>
2015-03-12 22:33:07 +0100

    "Search" saneRenamix.sh

--- testing/test.sh
+++ testing/test.sh
@@ -8,11 +8,17 @@ files=(
 	["Bibi_Blocksberg_15.03.07_08-30_zdf_30_TVOON_DE.mpg.avi"]="Bibi.Blocksberg..S01E06..Bibi.im.Dschungel.avi"			# Remove 3 words from the beginning
 );
 
+if [ -f test.sh ]; then
+	sr="../saneRenamix.sh"
+else
+	sr="./saneRenamix.sh"
+fi
+
 
 i=0
 for file in "${!files[@]}"; do
 	echo -e "\033[37m${file}";
-	result="$(./saneRenamix.sh $srArgs -s -f "$file")";
+	result="$($sr $srArgs -s -f "$file")";
 	if [ "$result" != "${files["$file"]}" ]; then
 		echo -e "\033[31m$file -> $result";
 		echo "'$result' != '${files[$file]}'";

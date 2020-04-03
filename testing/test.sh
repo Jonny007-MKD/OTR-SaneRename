1a9f9772cea921c43e9b80e524226f8ce941f2b3
Jonny007-MKD <ns-github-73478@jonny007-mkd.de>
2020-04-03 22:20:16 +0200

    Added testing EPGs

--- testing/test.sh
+++ testing/test.sh
@@ -7,22 +7,22 @@ declare -A files
 
 files=(
 # Nothing special
-	["Inspector_Barnaby_15.03.09_21-45_zdfneo_100_TVOON_DE.mpg.HQ.avi"]="Inspector.Barnaby..S07E02..Immer.wenn.der.Scherenschleifer....HQ.avi"
+	["Inspector_Barnaby_15.03.09_21-45_zdfneo_100_TVOON_DE.mpg.HQ.avi"]="Inspector.Barnaby..S07E02..Immer.wenn.der.Scherenschleifer....mpg.HQ.avi"
 
 # Remove 3 words from the beginning and some from the end
-	["Bibi_Blocksberg_15.03.07_08-30_zdf_30_TVOON_DE.mpg.avi"]="Bibi.Blocksberg..S01E06..Bibi.im.Dschungel.avi"
+	["Bibi_Blocksberg_15.03.07_08-30_zdf_30_TVOON_DE.mpg.avi"]="Bibi.Blocksberg..S01E06..Bibi.im.Dschungel.mpg.avi"
 # Use season and episode information from the beginning
-	["S05_E22_Good_Wife_15.03.11_00-40_sixx_40_TVOON_DE.mpg.HQ.avi"]="Good.Wife..S05E22..Ein.seltsames.Jahr.HQ.avi"
+	["S05_E22_Good_Wife_15.03.11_00-40_sixx_40_TVOON_DE.mpg.HQ.avi"]="Good.Wife..S05E22..Ein.seltsames.Jahr.mpg.HQ.avi"
 # Use season and episode information from the end
-	["Bibi_Blocksberg_S02E07_15.04.05_07-20_zdf_25_TVOON_DE.mpg.avi.otrkey"]="Bibi.Blocksberg..S02E07..Der.Hexengeburtstag.otrkey"
+	["Bibi_Blocksberg_S02E07_15.04.05_07-20_zdf_25_TVOON_DE.mpg.avi.otrkey"]="Bibi.Blocksberg..S02E07..Der.Hexengeburtstag.mpg.avi.otrkey"
 # Cut 3 words from the end
-	["Good_Wife_15.03.11_00-40_sixx_40_TVOON_DE.mpg.HQ.avi.otrkey"]="Good.Wife..S05E22..Ein.seltsames.Jahr.otrkey"
+	["Good_Wife_15.03.11_00-40_sixx_40_TVOON_DE.mpg.HQ.avi.otrkey"]="Good.Wife..S05E22..Ein.seltsames.Jahr.mpg.HQ.avi.otrkey"
 # Use , as delimiter
-	["H2O_Ploetzlich_Meerjungfrau_15.02.28_11-55_orf1_30_TVOON_DE.mpg.avi.otrkey"]="H2O.-.Plötzlich.Meerjungfrau..S03E06..Bella.irrt.otrkey"
+	["H2O_Ploetzlich_Meerjungfrau_15.02.28_11-55_orf1_30_TVOON_DE.mpg.avi.otrkey"]="H2O.-.Plötzlich.Meerjungfrau..S03E06..Bella.irrt.mpg.avi.otrkey"
 # Series and episode name in title
-	["Irene_Huss_Kripo_Goeteborg_Der_im_Dunkeln_wacht_S02E01_15.08.08_22-55_ard_90_TVOON_DE.mpg.HQ.avi"]="Irene.Huss,.Kripo.Göteborg..S02E01..Der.im.Dunkeln.wacht.HQ.avi"
+	["Irene_Huss_Kripo_Goeteborg_Der_im_Dunkeln_wacht_S02E01_15.08.08_22-55_ard_90_TVOON_DE.mpg.HQ.avi"]="Irene.Huss,.Kripo.Göteborg..S02E01..Der.im.Dunkeln.wacht.mpg.HQ.avi"
 # Difficult search for series
-	["Ein_Fall_fuer_TKKG_S01E01_15.11.16_13-20_kika_20_TVOON_DE.mpg.avi"]="Ein.Fall.für.TKKG..S01E01..Das.leere.Grab.im.Moor.avi"
+	["Ein_Fall_fuer_TKKG_S01E01_15.11.16_13-20_kika_20_TVOON_DE.mpg.avi"]="Ein.Fall.für.TKKG..S01E01..Das.leere.Grab.im.Moor.mpg.avi"
 );
 
 if [ -f test.sh ]; then
@@ -31,8 +31,8 @@ else
 	path="."
 fi
 
-if [ ! -f "$path/saneRenamix.sh" ]; then
-	echo "saneRenamix.sh not found!" >&2
+if [ ! -f "$path/saneRenamix.py" ]; then
+	echo "saneRenamix.py not found!" >&2
 fi
 
 
@@ -55,7 +55,7 @@ for the_file in "${!files[@]}"; do
 		ln -s "testing/$epg_file" "$path/$epg_file"
 	fi
 
-	result="$($path/saneRenamix.sh $srArgs -s -f "$the_file")";
+	result="$(python3 $path/saneRenamix.py $srArgs -s -f "$the_file")";
 	if [ "$result" != "${files["$the_file"]}" ]; then
 		echo -e "\033[31m$the_file -> $result (cache)";
 		echo "'$result' != '${files[$the_file]}'";
@@ -64,4 +64,6 @@ for the_file in "${!files[@]}"; do
 			rm $path/$epg_file
 		fi
 	fi
+	read -p "ok. next? "
 done;
+
